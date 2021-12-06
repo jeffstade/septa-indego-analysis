fc69b7ec32fe62632654b8307def1a6ccbb6f9b1
Jeff Stern <jeffstern.ny@gmail.com>
2021-12-06 12:25:58 -0500

    update templates

--- airflow/dags/philadelphia_pipeline/render_report.py
+++ airflow/dags/philadelphia_pipeline/render_report.py
@@ -24,13 +24,18 @@ def render_station_pages(template, station_data):
         mapdata_df = pd.read_gbq(f"SELECT * FROM `musa509-lab09.finalproj.indego_neighbor_stations` WHERE station_id='{row.station_id}'")
         mapdata_df.the_geom = gpd.GeoSeries.from_wkt(mapdata_df.the_geom)
         mapdata_gdf = gpd.GeoDataFrame(mapdata_df, geometry='the_geom')
+        neighbors_nunique = mapdata_df[['neighbor_route', 'neighbor_modal','neighbor_station']].groupby('neighbor_modal').nunique()
+        neighbors_count = mapdata_df[['neighbor_route', 'neighbor_modal','neighbor_station']].groupby('neighbor_modal').count()
         print(mapdata_df)
         output_station = template.render(
             station_name = row['name'],
             station_address = row['address'],
             lon = row['lon'],
             lat = row['lat'],
-            mapdata = mapdata_gdf.to_json())
+            mapdata = mapdata_gdf.to_json(),
+            neighbors_count = neighbors_count.to_json(),
+            neighbors_nunique = neighbors_nunique.to_json(),
+            nearby_bikes = neighbors_count['neighbor_station']['Bike'])
         pagename = row.station_id + '.html'
         page_location = str(output_root) + '/' + pagename
         with open(page_location, mode='w+') as outfile:
